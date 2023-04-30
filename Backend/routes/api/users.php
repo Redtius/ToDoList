2c2cf42b27a985bae7da2f1caa4b24fd9e6ff40d
Redtius <redtius@gmail.com>
2023-04-30 19:29:47 +0100

    added login

--- Backend/routes/api/users.php
+++ Backend/routes/api/users.php
@@ -5,5 +5,12 @@ use Illuminate\Support\Facades\Route;
 
 //Public routes
 Route::post('/register',[AuthController::class,'register'])->name('register');
+Route::post('/login',[AuthController::class,'login'])->name('login');
+
+//Protected routes
+Route::group(['middleware'=>'auth:sanctum'],function(){
+    Route::post('/logout',[AuthController::class,'logout'])->name('logout');
+});
+
 
 
